b9b0b14858aac387338376497dbc426e8d32b1a6
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-03-23 15:07:15 +0530

    fixed the typos in the asyncHandler and created the user controller file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-import express from "express";
-
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
+
+//necessary middlewares
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 console.log(process.env.PORT);
 export { app };
--- src/utils/AsyncHandler.ts
+++ src/utils/AsyncHandler.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { ControllerType } from "../types/types.js";
 
-export const AsyncHandler = (functionHandler: ControllerType) => {
+export const asyncHandler = (functionHandler: ControllerType) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(functionHandler(req, res, next)).catch((err) => next(err));
   };
