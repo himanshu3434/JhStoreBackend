1142434566cd310c64cae7c9bdc88c9a01faa8d6
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-03-26 20:47:00 +0530

    add routes for all  User Controllers

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,37 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  getCurrentUser,
+  loginUser,
+  refreshAccessToken,
+  registerUser,
+  updateAddress,
+  updateDob,
+  updateEmail,
+  updateGender,
+  updateName,
+  updateNumber,
+  updatePassword,
+  updateRole,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyAdmin } from "../middlewares/admin.middleware.js";
 
 const userRouter = Router();
 
-userRouter.route("/registerUser").post(registerUser);
-userRouter.route("/login").post(loginUser);
+userRouter.post("/registerUser", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/refreshAccessToken", verifyJWT, refreshAccessToken);
+userRouter.get("/getCurrentUser", verifyJWT, getCurrentUser);
+
+userRouter.post("/updateEmail", verifyJWT, updateEmail);
+userRouter.post("/updatePassword", verifyJWT, updatePassword);
+userRouter.post("/updateNumber", verifyJWT, updateNumber);
+userRouter.post("/updateAddress", verifyJWT, updateAddress);
+userRouter.post("/updateDob", verifyJWT, updateDob);
+userRouter.post("/updateName", verifyJWT, updateName);
+userRouter.post("/updateGender", verifyJWT, updateGender);
+userRouter.post("/deleteUser", verifyJWT, deleteUser);
+userRouter.post("/updateRole", verifyJWT, verifyAdmin, updateRole);
 
 export { userRouter };
