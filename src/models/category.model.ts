9762e83a97fe9580950c0e9a621564f9040e3445
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-03-26 18:17:01 +0530

    created   category schema and model

---
+++ src/models/category.model.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Category Name is required"],
+    unique: [true, "Category Name must be Unique"],
+  },
+});
+
+export const Category = mongoose.model("Category", CategorySchema);
