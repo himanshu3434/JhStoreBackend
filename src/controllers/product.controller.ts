e2fa1e26d76127708e488d2d84406bcf2f8ded37
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-03-26 23:19:09 +0530

    created product controller file and declared controller function (logic is not written )

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,7 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getAllProducts = asyncHandler(async (req, res) => {});
+
+const getProduct = asyncHandler(async (req, res) => {});
+
+//admin only routes
