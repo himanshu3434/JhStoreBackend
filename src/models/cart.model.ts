af78297dc5f8995685477e963c333f4e8f2d7b7a
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-03-26 18:20:36 +0530

    created cart schema and model

---
+++ src/models/cart.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const CartSchema = new Schema({
+  product_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+  },
+});
+
+export const Cart = mongoose.model("Cart", CartSchema);
