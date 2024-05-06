5fafabb42629dbe4513312b76a32670791f9bbec
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-05-06 18:28:33 +0530

    fixed payment integration as per regulation bug

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -4,7 +4,29 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const createPaymentIntent = asyncHandler(async (req, res) => {
   const { amount } = req.body;
+
+  console.log("amount  ", amount);
+  if (!amount)
+    return res
+      .status(404)
+      .json(new apiResponse(false, 404, null, "Amount is Required"));
+
+  const customer = await stripe.customers.create({
+    name: "Raju Rastogi",
+    email: "example@example.com",
+    address: {
+      city: "kasokaba",
+      country: "japan",
+      line1: "shinchan streeet",
+      line2: "",
+      postal_code: "226005",
+      state: "tokyo",
+    },
+  });
+
   const paymentIntent = await stripe.paymentIntents.create({
+    description: "lo regulation",
+    customer: customer.id,
     amount: amount,
     currency: "inr",
     automatic_payment_methods: {
