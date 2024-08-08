019deff3bbc2e096cea6333df2454d33cc1d4eb0
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-08-08 14:44:05 +0530

    fix other change

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -1,23 +1,23 @@
+import { NextFunction, Response } from "express";
 import { CustomRequest } from "../types/types.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const verifyAdmin = asyncHandler(
-  async (req: CustomRequest, res, next) => {
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
     const user = req.user;
     if (user?.role === "admin") {
       next();
-    }
-    else
-    return res
-      .status(401)
-      .json(
-        new apiResponse(
-          false,
-          401,
-          null,
-          "Unauthorised  request (Only Admin is Allowed )"
-        )
-      );
+    } else
+      return res
+        .status(401)
+        .json(
+          new apiResponse(
+            false,
+            401,
+            null,
+            "Unauthorised  request (Only Admin is Allowed )"
+          )
+        );
   }
 );
