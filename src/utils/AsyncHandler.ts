4a25d865d7ddf0eb4ab4535257bf2af2ea9ac39b
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-08-08 17:09:01 +0530

    Clear cache

--- src/utils/AsyncHandler.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import { ControllerType } from "../types/types.js";
-
-export const asyncHandler = (functionHandler: ControllerType) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(functionHandler(req, res, next)).catch((err) => next(err));
-  };
-};
