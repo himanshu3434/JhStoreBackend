26d21eea876c508521d6bd7e7689a92aeee38f50
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-09-22 16:38:40 +0530

    updated cors origin

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [process.env.CORS_ORIGIN || ""],
     methods: ["POST", "GET", "PUT", "DELETE"],
 
     credentials: true,
